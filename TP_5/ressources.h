9aca2131000ffa65626e4e71dd670554c2f1d07e
pBouillon <pierre.bouillon@openmailbox.org>
2017-12-07 15:53:36 +0100

    commented constants

--- TP_5/ressources.h
+++ TP_5/ressources.h
@@ -1,27 +1,27 @@
 #ifndef RESSOURCES_H_
 #define RESSOURCES_H_
 
-#define  ARGS_READER     5
-#define  ARGS_MAIN       4
-#define  MAX_FORKS       100
+#define  ARGS_READER     5   /*!< args required for the file_parser */
+#define  ARGS_MAIN       4   /*!< args required for the main  */
+#define  MAX_FORKS       100 /*!< fork limit */
 
-#define  KEY_FILE        "./file_parser.c"
-#define  KEY_ID          42
+#define  KEY_FILE        "./file_parser.c" /*!< file used for ftok */
+#define  KEY_ID          42                /*!< key for ftok*/
 
-#define  BUFF_SIZE       1024
-#define  RES_FILE_NAME   100
+#define  BUFF_SIZE       1024 /*!< Buffer size for reading */
+#define  RES_FILE_NAME   100  /*!< Buffer for the output file name */
 
-#define  SEPARATORS      " ,;:.!?\n"
-#define  SAVE_LIMIT      10000
+#define  SEPARATORS      " ,;:.!?\n" /*<! Separators when reading */
+#define  SAVE_LIMIT      10000       /*<! Lines read before save */
 
-#define  WORDS_LEN       30
+#define  WORDS_LEN       30   /*<! Max words per lines */
 
-#define  SHM_ID          1337
-#define  NO_OPT          "0"
+#define  SHM_ID          1337 /*<! Shared memory ID */
+#define  NO_OPT          "0"  /*<! nopt for execl (ex part 2.1) */
 
-#define  SEM_ID          0
-#define  SEM_NB          1
-#define  SEM_TOKEN_NB    1
+#define  SEM_ID          0    /*<! semaphore ID */
+#define  SEM_NB          1    /*<! semaphore number */
+#define  SEM_TOKEN_NB    1    /*<! semaphore token amount*/
 
 int  get_file_lines (char* filename) ;
 
